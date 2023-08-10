02dcd2fce6bc0a889e2ad218ce68d5489fdcba0f
Sameer Limbu <sameerlimbu31@gmail.com>
2023-08-10 23:29:24 +0100

    v 1.0 Final product
    Early final product released

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ const app = Vue.createApp({
             
         }
     },
-   
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+            
+        },
+    },
 })
 
 app.mount('#app')
