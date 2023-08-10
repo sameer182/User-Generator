41e5c42f2cf9af3bb36efde7ca56e3c7ae586d04
Sameer Limbu <sameerlimbu31@gmail.com>
2023-08-10 23:08:13 +0100

    added user

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const app = Vue.createApp({
-    template: "<h1>Hello Sameer</h1>",
+    data() {
+        return {
+            firstName: 'Sameer',
+            lastName: 'Limbu',
+            email: 'sam@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+            
+        }
+    },
+   
 })
 
 app.mount('#app')
